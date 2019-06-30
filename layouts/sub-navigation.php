2432ff7948e1a2da5d7be833fd2591ba217e8849
Gian Carlo Egamino <gnegamino@gmail.com>
2019-06-30 22:57:11 +0800

    :star: Added About UI

---
+++ layouts/sub-navigation.php
@@ -0,0 +1,54 @@
+<div id="sub-navigation-bar" class="navigation-bar">
+    <div class="client-contact">
+        <div class="client-contact-item">
+            <span class="fa fa-envelope"></span>
+            <span class="client-contact-text">info@maesthetics.com</span>
+        </div>
+        <div class="client-contact-item">
+            <span class="fa fa-phone"></span>
+            <span class="client-contact-text">(+632) 756 2154</span>
+        </div>
+        <div class="client-contact-item">
+            <span class="fa fa-mobile"></span>
+            <span class="client-contact-text">(+63917) 829 6319</span>
+        </div>
+    </div>
+    <div class="container">
+        <div class="sub-navigation-bar">
+            <div class="left">
+                <a to="/">
+                    <div class="sub-client-logo">
+                        <img class="client-logo-img" src="/assets/images/client-logo.png">
+                    </div>
+                </a>
+                <div class="navigation-bar-group">
+                    <div class="navigation-bar-content">
+                        <a href="about.php">
+                            About
+                        </a>
+                    </div>
+                    <div class="navigation-bar-content">
+                        <a href="services.php">
+                            Services
+                        </a>
+                    </div>
+                    <div class="navigation-bar-content">
+                        <a href="contact.php">
+                            Contact Us
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="navigation-bar-group social-media">
+                    <div class="social-media-link facebook">
+                        <i class="fa fa-facebook-square fa-2x"></i>
+                    </div>
+                    <div class="social-media-link instagram">
+                        <i class="fa fa-instagram fa-2x"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
