ce6639f371343d22dd7926c8b96d061e8144480a
Gian Carlo Egamino <giancarloegamino.bonfire@gmail.com>
2019-07-01 21:24:34 +0800

    :star: Update responsiveness of Home UI

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <div id="app">
     <div class="home">
+        <?php require_once('layouts/sub-navigation.php'); ?>
         <div class="background bg-initial"></div>
         <div class="intro-container">
             <div class="intro-text">More Confident</div>
@@ -28,62 +29,6 @@
                 <a href="contact.php">
                     Contact Us
                 </a>
-            </div>
-        </div>
-        <div id="sub-navigation-bar" class="navigation-bar">
-            <div class="client-contact">
-                <div class="client-contact-item">
-                    <span class="fa fa-envelope"></span>
-                    <span class="client-contact-text">info@maesthetics.com</span>
-                </div>
-                <div class="client-contact-item">
-                    <span class="fa fa-phone"></span>
-                    <span class="client-contact-text">(+632) 962 5744</span>
-                </div>
-                <div class="client-contact-item">
-                    <span class="fa fa-mobile"></span>
-                    <span class="client-contact-text">(+639) 255 36 3651</span>
-                </div>
-            </div>
-            <div class="container">
-                <div class="sub-navigation-bar">
-                    <div class="left">
-                        <a href="index.php">
-                            <div class="sub-client-logo">
-                                <img class="client-logo-img" src="/assets/images/client-logo.png">
-                            </div>
-                        </a>
-                        <div class="navigation-bar-group">
-                            <div class="navigation-bar-content">
-                                <a href="about.php">
-                                    About
-                                </a>
-                            </div>
-                            <div class="navigation-bar-content">
-                                <a href="services.php">
-                                    Services
-                                </a>
-                            </div>
-                            <div class="navigation-bar-content">
-                                <a href="contact.php">
-                                    Contact Us
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="right">
-                        <div class="navigation-bar-group social-media">
-                            <div class="social-media-link facebook">
-                                <a target="_blank" href="https://www.facebook.com/maestheticsclinic">
-                                    <i class="fa fa-facebook-square fa-2x"></i>
-                                </a>
-                            </div>
-                            <div class="social-media-link instagram">
-                                <i class="fa fa-instagram fa-2x"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <div class="book-now">
@@ -222,3 +167,4 @@
 <?php require_once('layouts/master_footer.php'); ?>
 
 <script src="/assets/js/index.js"></script>
+<script src="/assets/js/mini-navigation.js"></script>
