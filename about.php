bc01b882d0e6e61beb0d249b20d6decd118d0f11
Gian Carlo Egamino <giancarloegamino.bonfire@gmail.com>
2019-07-03 22:57:17 +0800

    :star: Revised Home and About Pages

--- about.php
+++ about.php
@@ -1,66 +1,51 @@
-<?php require_once('layouts/master_header.php'); ?>
+<?php
+    require_once('layouts/master_header.php');
+    require_once('layouts/sub-navigation.php');
+?>
 
-<div id="app">
-    <div class="about pushed-content">
-        <?php require_once('layouts/sub-navigation.php'); ?>
-        <div class="regular-content parallax-2">
-            <div class="container">
-                <div class="hero-title hero-title--left">
-                    <h1>OUR COMPANY</h1>
-                    <h2>Your beauty, our expertise.</h2>
-                    <h2>Allure and shine!</h2>
-                </div>
-            </div>
+<div class="about">
+    <div class="page-banner">
+        <h1>OUR COMPANY</h1>
+        <div class="motto">
+            <h2>More Confident</h2>
+            <h2>●</h2>
+            <h2>More Beautiful</h2>
+            <h2>●</h2>
+            <h2>More You</h2>
         </div>
-        <div class="content-section section--slate">
-            <div class="container">
-                <div class="content-header">
-                    WHO ARE WE
+    </div>
+    <div class="page-section">
+        <div class="container">
+            <div class="company-profile">
+                <div class="client-logo">
+                    <img class="client-logo-img" src="/assets/images/client-logo.png">
                 </div>
-                <p class="content-paragraph">
-                    M Aesthetics Clinic is a world class facility which offer various products and procedures designed to enhance one's beauty and fulfill one's fantasy of magnificence. Procedures offered include Facials, Drips and Lasers. We are equipped with brandnew, state of the art machines that produce exceptional results. Our center also houses an Operating Theater designed to accommodate all aesthetic surgeries. Patients can rest in our Recovery Room after each surgery.
+                <p>
+                    <span class="primary">M Aesthetics Clinic</span> is a world class facility which offer various products and procedures designed to enhance one's beauty and fulfill one's fantasy of magnificence. Procedures offered include Facials, Drips and Lasers. We are equipped with brandnew, state of the art machines that produce exceptional results. Our center also houses an Operating Theater designed to accommodate all aesthetic surgeries. Patients can rest in our Recovery Room after each surgery.
                 </p>
-                <p class="content-paragraph">
+                <p>
                     Our Surgeon is a member of the Philippine Association of Plastic, Reconstructive and Aesthetic Surgeons (PAPRAS), the only society recognized by the Philippine Medical Association. This recognition assures you the best results. 
                 </p>
-                <p class="content-paragraph">
+                <p>
                     We are located in the heart of Quezon City's business district with ample parking and valet service. Easy access to public transporation is also available. We can also arrange accomodation, recreation and transportation for patients.
                 </p>
             </div>
         </div>
-        <div class="content-section section--light">
-            <div class="owner-section">
-                <div class="container">
-                    <div class="doctor-mike">
-                        <img class="doctor-mike-image" src="/assets/images/mike-versoza.jpeg">
-                        <div class="doctor-mike-details">
-                            <h2>Dr. Michael "MIKE" Verzosa, M.D., D.P.B.P.S</h2>
-                            <h3>Board Certified Plastic Surgeon</h3>
-                            <h3>Volunteer, Operation Smile</h3>
-                            <h3>Volunteer, Smile Train</h3>
-                            <div class="doctor-mike-affiliations">
-                                <h3>Affiliations:</h3>
-                                <h6>Philippine Association of Plastic Reconstructive and Aesthetic Surgeons (PAPRAS)</h6>
-                                <img src="/assets/images/papras.png">
-                            </div>
-                        </div>
+    </div>
+    <div class="owner-section">
+        <div class="container">
+            <div class="doctor-mike">
+                <img class="doctor-mike-image" src="/assets/images/mike-versoza.jpeg">
+                <div class="doctor-mike-details">
+                    <h2>Dr. Michael "MIKE" Verzosa, M.D., D.P.B.P.S</h2>
+                    <h3>Board Certified Plastic Surgeon</h3>
+                    <h3>Volunteer, Operation Smile</h3>
+                    <h3>Volunteer, Smile Train</h3>
+                    <div class="doctor-mike-affiliations">
+                        <h3>Affiliations:</h3>
+                        <h6>Philippine Association of Plastic Reconstructive and Aesthetic Surgeons (PAPRAS)</h6>
+                        <img src="/assets/images/papras.png">
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="content-section section--slate">
-            <div class="container">
-                <div class="content-header">
-                    WHERE WE DO IT
-                </div>
-                <p class="content-paragraph">
-                    Our clinic was designed to be warm and welcoming, with emphasis on comfort. We want your experience to be as pleasant as we can possibly make it. You enter with the feeling of ease and from entry to exit it is our goal that each of our valued patients has a sense that a meaningful and trustworthy relationship has been formed between our establishment and yourself.
-                </p>
-                <div class="gallery">
-                    <img src="/assets/images/place1.jpg">
-                    <img src="/assets/images/place2.jpg">
-                    <img src="/assets/images/place3.jpg">
-                    <img src="/assets/images/place4.jpg">
                 </div>
             </div>
         </div>
