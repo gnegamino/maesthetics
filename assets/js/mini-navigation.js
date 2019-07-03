4ea4c1cda4ea50026cbf3aa1010d41ff09a7a1c9
Gian Carlo Egamino <giancarloegamino.bonfire@gmail.com>
2019-07-03 23:44:45 +0800

    :star: Revamp Services and Contact UI

--- assets/js/mini-navigation.js
+++ assets/js/mini-navigation.js
@@ -10,4 +10,10 @@ $(function(){
             $('.mini-dropdown-navigation .dropdown-navigation').slideUp();
         }
     });
+
+    $("#surgeries").mouseover(function() {
+        $('.page-banner').css('background-image', 'url("/assets/images/static/breast-1.JPG")');
+    }).mouseout(function() {
+        $('.page-banner').css('background-image', 'url("/assets/images/static/bg-black-1.JPG")');
+    });
 });
