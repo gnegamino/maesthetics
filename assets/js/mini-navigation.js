2a48d5b74355bb867d950640cf0d27d4a408450f
Gian Carlo Egamino <giancarloegamino.bonfire@gmail.com>
2019-07-04 12:59:56 +0800

    :star: Revamp Services UI

--- assets/js/mini-navigation.js
+++ assets/js/mini-navigation.js
@@ -12,7 +12,19 @@ $(function(){
     });
 
     $("#surgeries").mouseover(function() {
-        $('.page-banner').css('background-image', 'url("/assets/images/static/breast-1.JPG")');
+        $('.page-banner').css('background-image', 'url("/assets/images/static/bg-surgeries.JPG")');
+    }).mouseout(function() {
+        $('.page-banner').css('background-image', 'url("/assets/images/static/bg-black-1.JPG")');
+    });
+
+    $("#face-skin-body").mouseover(function() {
+        $('.page-banner').css('background-image', 'url("/assets/images/static/bg-face-body.JPG")');
+    }).mouseout(function() {
+        $('.page-banner').css('background-image', 'url("/assets/images/static/bg-black-1.JPG")');
+    });
+
+    $("#lasers-machines").mouseover(function() {
+        $('.page-banner').css('background-image', 'url("/assets/images/static/bg-lasers.JPG")');
     }).mouseout(function() {
         $('.page-banner').css('background-image', 'url("/assets/images/static/bg-black-1.JPG")');
     });
--- services.php
+++ services.php
@@ -7,74 +7,12 @@
     <div class="page-banner">
         <h1>OUR SERVICES</h1>
         <div class="services-buttons">
-            <button id="surgeries" class="btn">SURGERIES</button>
-            <button id="face-skin-body" class="btn">FACE, SKIN AND BODY</button>
-            <button id="lasers-machines" class="btn">LASERS AND MACHINES</button>
+            <a href="surgeries.php" id="surgeries" class="btn">SURGERIES</a>
+            <a href="face-skin-body.php" id="face-skin-body" class="btn">FACE, SKIN AND BODY</a>
+            <a href="lasers-machines.php" id="lasers-machines" class="btn">LASERS AND MACHINES</button>
         </div>
     </div>
 </div>
-
-
-<!-- <div class="regular-content parallax-3">
-    <div class="container">
-        <div class="hero-title hero-title--right">
-            <h1>SERVICES</h1>
-            <h2>Feeling tired and stressed? No worries! A surprise is waiting for you! <br>Pamper yourself with our extraordinary services done by our experts.</h2>
-        </div>
-    </div>
-</div>
-<div class="content-section section--light">
-    <div class="container">
-        <div class="content-and-picture">
-            <img src="/assets/images/girl1.jpg">
-            <div class="detail">
-                <div class="content-sub-header">
-                    FAT TRANSFER FILLERS
-                </div>
-                <p class="content-paragraph">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ea cum quisquam eaque in placeat omnis dolore? Quae hic atque quisquam, earum nulla quod minima, maxime velit sunt dicta cupiditate!
-                </p>
-                <button type="button" class="detail-button">VIEW MORE</button>
-            </div>
-        </div>
-        <div class="content-and-picture">
-            <img src="/assets/images/girl1.jpg">
-            <div class="detail">
-                <div class="content-sub-header">
-                    FAT TRANSFER FILLERS
-                </div>
-                <p class="content-paragraph">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ea cum quisquam eaque in placeat omnis dolore? Quae hic atque quisquam, earum nulla quod minima, maxime velit sunt dicta cupiditate!
-                </p>
-                <button type="button" class="detail-button">VIEW MORE</button>
-            </div>
-        </div>
-        <div class="content-and-picture">
-            <img src="/assets/images/girl1.jpg">
-            <div class="detail">
-                <div class="content-sub-header">
-                    FAT TRANSFER FILLERS
-                </div>
-                <p class="content-paragraph">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ea cum quisquam eaque in placeat omnis dolore? Quae hic atque quisquam, earum nulla quod minima, maxime velit sunt dicta cupiditate!
-                </p>
-                <button type="button" class="detail-button">VIEW MORE</button>
-            </div>
-        </div>
-        <div class="content-and-picture">
-            <img src="/assets/images/girl1.jpg">
-            <div class="detail">
-                <div class="content-sub-header">
-                    FAT TRANSFER FILLERS
-                </div>
-                <p class="content-paragraph">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ea cum quisquam eaque in placeat omnis dolore? Quae hic atque quisquam, earum nulla quod minima, maxime velit sunt dicta cupiditate!
-                </p>
-                <button type="button" class="detail-button">VIEW MORE</button>
-            </div>
-        </div>
-    </div>
-</div> -->
 
 <?php require_once('layouts/master_footer.php'); ?>
 
