9834c37e342ab77a3ff48b62634dcb425907a814
Gian Carlo Egamino <giancarloegamino.bonfire@gmail.com>
2019-07-04 17:30:42 +0800

    :star: Added services UI

---
+++ surgeries.php
@@ -0,0 +1,126 @@
+<?php
+    require_once('layouts/master_header.php');
+    require_once('layouts/sub-navigation.php');
+?>
+
+<div class="services surgeries">
+    <div class="page-banner">
+        <h1>SURGERIES</h1>
+    </div>
+    <div class="media-section">
+        <div class="container">
+            <h1>FEATURED SERVICES</h1>
+            <div class="media-list">
+                <div class="media-item">
+                    <div class="media-picture">
+                        <img src="/assets/images/static/breast-1.JPG" alt="">
+                    </div>
+                    <div class="media-detail">
+                        <div class="media-title">Rhinoplasty</div>
+                        <div class="media-description">
+                            This is a facial cosmetic procedure that is usually performed to enhance the appearance of the nose. During this type of rhinoplasty, the nasal cartilage and bones are modified, or tissue is added.
+                        </div>
+                        <div class="media-button">
+                            <button class="btn view-more">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="media-item">
+                    <div class="media-picture">
+                        <img src="/assets/images/static/breast-1.JPG" alt="">
+                    </div>
+                    <div class="media-detail">
+                        <div class="media-title">Breast Augmentation</div>
+                        <div class="media-description">
+                            Also known as augmentation mammoplasty — is surgery to increase breast size. It involves placing breast implants under breast tissue or chest muscles.
+                        </div>
+                        <div class="media-button">
+                            <button class="btn view-more">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="media-item">
+                    <div class="media-picture">
+                        <img src="/assets/images/static/breast-1.JPG" alt="">
+                    </div>
+                    <div class="media-detail">
+                        <div class="media-title">Tummy Tuck</div>
+                        <div class="media-description">
+                            A tummy tuck, also known as abdominoplasty, removes excess fat and skin and restores weakened muscles to create a smoother, firmer abdominal profile.
+                        </div>
+                        <div class="media-button">
+                            <button class="btn view-more">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="media-item">
+                    <div class="media-picture">
+                        <img src="/assets/images/static/breast-1.JPG" alt="">
+                    </div>
+                    <div class="media-detail">
+                        <div class="media-title">Liposuction / Cavi Lipo</div>
+                        <div class="media-description">
+                            A surgical procedure that uses a suction technique to remove fat from specific areas of the body, such as the abdomen, hips, thighs, buttocks, arms or neck. Liposuction also shapes (contours) these areas.
+                        </div>
+                        <div class="media-button">
+                            <button class="btn view-more">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="media-item">
+                    <div class="media-picture">
+                        <img src="/assets/images/static/breast-1.JPG" alt="">
+                    </div>
+                    <div class="media-detail">
+                        <div class="media-title">Gynecomastia Surgery</div>
+                        <div class="media-description">
+                            Also known as male breast reduction, is the surgical correction of overdeveloped or enlarged breasts in men.
+                        </div>
+                        <div class="media-button">
+                            <button class="btn view-more">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="media-item">
+                    <div class="media-picture">
+                        <img src="/assets/images/static/breast-1.JPG" alt="">
+                    </div>
+                    <div class="media-detail">
+                        <div class="media-title">FTM</div>
+                        <div class="media-description">
+                            This is a facial cosmetic procedure that is usually performed to enhance the appearance of the nose. During this type of rhinoplasty, the nasal cartilage and bones are modified, or tissue is added.
+                        </div>
+                        <div class="media-button">
+                            <button class="btn view-more">View More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="all-services-section">
+        <div class="container">
+            <h1>ALL SURGERIES</h1>
+            <div class="media-box">
+                <div class="media-box-item">
+                    <div class="media-box-header">
+                        <div class="media-box-header-icon"></div>
+                        <div class="media-box-header-title">AESTHETICS</div>
+                    </div>
+                    <div class="media-box-content"></div>
+                </div>
+                <div class="media-box-item">
+                    <div class="media-box-header">
+                        <div class="media-box-header-icon"></div>
+                        <div class="media-box-header-title">RECONSTRUCTIVE</div>
+                    </div>
+                    <div class="media-box-content"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('layouts/master_footer.php'); ?>
+
+<script src="/assets/js/mini-navigation.js"></script>
