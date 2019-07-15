3941f604d431835d644b2d0fe4fb0e56c25efe4f
jonelzape <jonel.zape@outlook.com>
2019-07-15 22:14:18 +0800

    :fire: Fixed update about info error

--- admin/backend/about-save-edit.php
+++ admin/backend/about-save-edit.php
@@ -2,6 +2,6 @@
 
 $content = escapeString(['content' => $_POST['data']]);
 
-runQuery("UPDATE `content` SET `content` = '".$content['content']."' WHERE `module` = 'about'", );
+runQuery("UPDATE `content` SET `content` = '".$content['content']."' WHERE `module` = 'about'");
 
 response(["message" => ""]);
