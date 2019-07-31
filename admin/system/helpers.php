87a22049e48fb1f66976a933290ac63f71098db1
jonelzape <jonel.zape@outlook.com>
2019-07-31 21:55:33 +0800

    :fire: Fixed image orientation bug upon upload of image

--- admin/system/helpers.php
+++ admin/system/helpers.php
@@ -94,3 +94,32 @@ function photoCountLabel($count)
 
     return $count." Photo";
 }
+
+function correctImageOrientation($filename)
+{
+    if (function_exists('exif_read_data')) {
+        $exif = @exif_read_data($filename);
+        if ($exif && isset($exif['Orientation'])) {
+            $orientation = $exif['Orientation'];
+            if ($orientation != 1) {
+                $img = imagecreatefromjpeg($filename);
+                $deg = 0;
+                switch ($orientation) {
+                case 3:
+                    $deg = 180;
+                    break;
+                case 6:
+                    $deg = 270;
+                    break;
+                case 8:
+                    $deg = 90;
+                    break;
+                }
+                if ($deg) {
+                    $img = imagerotate($img, $deg, 0);
+                }
+                imagejpeg($img, $filename, 95);
+            }
+        }
+    }
+}
